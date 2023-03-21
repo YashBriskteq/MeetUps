76e680b7d22652e50d40807b55f084991f3d45aa
Yash Pandya <yash.pandya@briskteq.com>
2023-03-21 17:59:44 +0530

    carddetails added

---
+++ src/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+function MeetupDetails() {
+  return (
+    <Fragment>
+      {/* <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="london-img" /> */}
+      <h1>london</h1>
+      <address>london, United kingdom</address>
+      <p>a place to visit once</p>
+    </Fragment>
+  );
+}
+
+export default MeetupDetails;
