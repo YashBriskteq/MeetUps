46f6428cae511fafee026abb94f73ed2acefb7d6
Yash Pandya <yash.pandya@briskteq.com>
2023-03-22 09:30:36 +0530

    css added

--- src/pages/[meetupId]/index.js
+++ src/pages/[meetupId]/index.js
@@ -1,13 +1,13 @@
-import { Fragment } from "react";
+import MeetupDetail from "../../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
   return (
-    <Fragment>
-      {/* <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="london-img" /> */}
-      <h1>london</h1>
-      <address>london, United kingdom</address>
-      <p>a place to visit once</p>
-    </Fragment>
+    <MeetupDetail
+      image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+      title="london"
+      address="london, United kingdom"
+      description="a place to visit once"
+    />
   );
 }
 
